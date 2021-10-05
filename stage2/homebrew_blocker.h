bcc8a3d8dfb6f32d07f515f65a3ad791470c1101
rhynec <86078431+rhynec@users.noreply.github.com>
2021-10-04 21:32:24 -0500

    Mamba 8.4
    - Blacklist homebrew NP00PKGI3 when CFW syscalls are disabled (allowed due NP*)

--- stage2/homebrew_blocker.h
+++ stage2/homebrew_blocker.h
@@ -178,7 +178,8 @@ static inline int block_homebrew(const char *path)
 				!strncmp(gameid, "BLES01337", 9) || // Awesome File Manager
 				!strncmp(gameid, "BLND00001", 9) || // dev_blind
 				!strncmp(gameid, "NPEA90124", 9) || // SEN Enabler
-				!strncmp(gameid, "NP0APOLLO", 9) // // Apollo Save Tool
+				!strncmp(gameid, "NP0APOLLO", 9) || // Apollo Save Tool
+				!strncmp(gameid, "NP00PKGI3", 9) // // pkgi-ps3
 				) allow = 0;
 
 			// test whitelist.cfg and blacklist.cfg
